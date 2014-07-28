9d0dc0cfcbd48bbf91fe4fb8a7624e3d385e6fd7
David Charboneau <david@adadabase.com>
2014-07-28 18:16:12 -0400

    Install fsharp and make sure R is discoverable.

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,16 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64_vmware.box"
+
+  config.vm.provider "vmware_fusion" do |v|
+      # This much ram is needed to build fsharp, you can drop it down once
+      # fsharp is built.
+      v.vmx["memsize"] = "8192"
+      v.vmx["numvcpus"] = "2"
+  end
+
   config.omnibus.chef_version = :latest
+
   config.vm.provision :chef_solo do |chef|
     chef.cookbooks_path = ["cookbooks","site-cookbooks"]
     chef.add_recipe "FSharpRProvider.DevEnv"
--- site-cookbooks/FSharpRProvider.DevEnv/recipes/default.rb
+++ site-cookbooks/FSharpRProvider.DevEnv/recipes/default.rb
@@ -2,8 +2,53 @@ include_recipe "apt"
 include_recipe "build-essential"
 include_recipe "mono"
 
-["r-base","r-base-dev"].each do |pkg|
+["r-base","r-base-dev","pkg-config","autoconf","libtool","automake"].each do |pkg|
   package pkg do
     action :install
   end
+end
+
+bash "Set the R_HOME variable so R libs are discoverable" do
+  user "root"
+  code <<-EOH
+  echo "export R_HOME=/usr/lib/R" > /etc/profile.d/r_home.sh
+  EOH
+end
+
+# This will make sure that mono libs are resolveable
+bash "Make sure ldconfig was run" do
+  user "root"
+  code <<-EOH
+  ldconfig
+  EOH
+end
+
+fsharp_installation_dir = "/usr/local/src/fsharp"
+
+git fsharp_installation_dir do
+  repository "https://github.com/fsharp/fsharp"
+  reference "master"
+  action :sync
+  notifies :run, "bash[build_fsharp]"
+end
+
+bash "build_fsharp" do
+  user "root"
+  cwd fsharp_installation_dir
+  code <<-EOH
+  ./autogen.sh --prefix /usr/local
+  make
+  make install
+  ldconfig
+  EOH
+end
+
+bash "update certificates for NuGet" do
+  user "root"
+  code <<-EOH
+  mozroots --import --machine --sync
+  yes | certmgr -ssl -m https://go.microsoft.com
+  yes | certmgr -ssl -m https://nugetgallery.blob.core.windows.net
+  yes | certmgr -ssl -m https://nuget.org
+  EOH
 end
